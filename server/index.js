86421610f1bcd72177698934069f845a017fd038
Tomohiro Akinaga <tomo@TomohironoMacBook-Pro.local>
2022-12-04 15:03:09 -0800

    added crypto-js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 8000;
 const dotenv = require("dotenv").config();
+const CryptoJS = require("crypto-js");
+const User = require("./src/v1/models/user");
 
 try {
     mongoose.connect(process.env.MONGODB_URL);
@@ -10,6 +12,19 @@ try {
 } catch (error) {
     console.log("error");
 }
+
+// User Registration API
+app.post("/register", async (req, res) => {
+    const password = req.body.password;
+
+    try {
+        req.body.password = CryptoJS.AES.encrypt(
+            password,
+            process.env.SECRET_KEY
+        );
+        const user = await User.create(req.body);
+    } catch {}
+});
 
 app.listen(PORT, () => {
     console.log("Server is ready");
